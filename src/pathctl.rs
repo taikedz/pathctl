c4e18fe7a3491431cd17766e9fbca353dda95728
Tai Kedzierski <dch.tai@gmail.com>
2023-09-05 21:32:37 +0100

    Successfully display the path, removing extraneous lines

--- src/pathctl.rs
+++ src/pathctl.rs
@@ -4,10 +4,28 @@ use std::path::Path;
 const PATHFILE_PATH :&str = "~/.PATH";
 
 fn main() {
-    let data:Vec<String> = read_lines(PATHFILE_PATH);
-    for line in data.iter() {
-        println!("{line}");
-    }
+    display_path();
+}
+
+fn display_path() {
+    let data:Vec<String> = load_lines(PATHFILE_PATH);
+    let resolved_searchpath = data.join(":");
+
+    println!("{resolved_searchpath}");
+}
+
+fn load_lines(raw_path:&str) -> Vec<String> {
+    read_lines(raw_path)
+        .iter() // Explicitly iterate
+        .map(|line| line.trim()) // trim() returns a &str , referencing the iterated line
+        .filter(|line| is_valid_line(line)) // line is the &str
+        .map(String::from) // because the current function owns the object, and we now only have a &str, we create a new object for returning
+        .collect::<Vec<String>>() // and finally create a vector, rather than a map . Note the "::" and the doubled '<'
+}
+
+
+fn is_valid_line(line:&str) -> bool {
+    return line.len() > 0 && &line[0..1] != "#";
 }
 
 
