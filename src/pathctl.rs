497e239b2a4e64b20cec71bcdda1f5f896a80ce0
Tai Kedzierski <dch.tai@gmail.com>
2023-09-05 20:52:19 +0100

    Simpler file reading, iteration over vector

--- src/pathctl.rs
+++ src/pathctl.rs
@@ -1,19 +1,29 @@
-use std::fs::File;
+use std::fs::read_to_string;
 use std::path::Path;
 
 const PATHFILE_PATH :&str = "~/.PATH";
 
 fn main() {
-    let path_str = format!("{}", shellexpand::tilde(PATHFILE_PATH));
+    let data:Vec<String> = read_lines(PATHFILE_PATH);
+    for line in data.iter() {
+        println!("{line}");
+    }
+}
+
+
+fn read_lines(raw_path: &str) -> Vec<String> {
+    let path_str = format!("{}", shellexpand::tilde(raw_path));
     let path = Path::new(&path_str);
 
-    if(!path.exists()) {
+    if !path.exists() {
         eprintln!("Could not find '{}'", path_str);
         std::process::exit(1);
     }
 
-    let file = match File::open(&path) {
-        Err(e) => panic!("File open error: {path_str}: {e}"),
-        Ok(file) => file,
-    };
+    // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+    read_to_string(path_str) 
+        .unwrap()           // panic on possible file-reading errors
+        .lines()            // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()          // gather them together into a vector
 }
