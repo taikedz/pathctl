155a0ed6f5aa6bbbf4dc9755e0ba41a22056e468
Tai Kedzierski <dch.tai@gmail.com>
2023-09-11 13:59:04 +0100

    Demonstrate using 'use' after 'mod'

--- src/pathctl.rs
+++ src/pathctl.rs
@@ -1,21 +1,25 @@
+// Since this is the main script, `util` and `version` can live sidecar to `pathctl.rs`
+//  If they lived in another file, say `mods.rs`, the declarations would cause these to
+//  need to live in `mods/util.rs` and `mods/version.rs`
 mod util;
 mod version;
 
+use util::*;
 
 fn main() {
     // Advanced: specify which ones have which lengths required
     let valid_args : Vec<&str> = vec!["load", "add", "version"];
 
-    let action = util::args::get_arguments(&valid_args);
+    let action = args::get_arguments(&valid_args);
 
     if action.label == "load" {
-        util::display::display_path();
+        display::display_path();
     }
     else if action.label == "version" {
         println!("{}", version::VERSION);
     }
     else if action.label == "add" {
-        util::append::append(&action.target);
+        append::append(&action.target);
     }
     else {
         eprintln!("Invalid action: {}", action.label);
